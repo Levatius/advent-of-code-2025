961df67d49e21b08f5eabc352fb5ef8592929262
Lev <levatius@hotmail.co.uk>
2025-12-03 20:44:49 +0000

    README tweaks

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -5,15 +5,19 @@ root = ElementTree.parse("tests-report.xml").getroot()
 
 rows = []
 for testcase in root.iter("testcase"):
-    day = re.match(r"test.test__day_(.+)", testcase.get("classname")).group(1)
     part, test = re.match(r"test__part_(.+)__(.+)", testcase.get("name")).groups()
+
+    if test != "puzzle_input":
+        continue
+
+    day = re.match(r"test.test__day_(.+)", testcase.get("classname")).group(1)
     time = float(testcase.get("time"))
 
-    rows.append((day, part, test, str(int(time * 1000))))
+    rows.append((day, part, str(int(time * 1000))))
 
 lines = [
-    "| Day | Part | Test | Time (ms) |\n",
-    "|-----|------|------|-----------|\n",
+    "| Day | Part | Time (ms) |\n",
+    "|-----|------|-----------|\n",
 ]
 for row in rows:
     lines.append(f"| {' | '.join(row)} |\n")
