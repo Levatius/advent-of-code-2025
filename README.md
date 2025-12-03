fdb5443f67765e11b8e68b4b511b977e903a6b06
Lev <levatius@hotmail.co.uk>
2025-12-03 20:28:51 +0000

    README tweaks

--- README.md
+++
@@ -1,3 +0,0 @@
-![Tests Status Badge](https://github.com/Levatius/advent-of-code-2025/actions/workflows/python-tests.yml/badge.svg)
-
-![Speed Report](https://gist.githubusercontent.com/Levatius/2aec0c88c0ad990e4e1dcff16c004ed9/raw/speed-report.md)
---
+++ scripts/generate_readme.py
@@ -0,0 +1,22 @@
+import re
+from xml.etree import ElementTree
+
+root = ElementTree.parse("tests-report.xml").getroot()
+
+rows = []
+for testcase in root.iter("testcase"):
+    day = re.match(r"test.test__day_(.+)", testcase.get("classname")).group(1)
+    part, test = re.match(r"test__part_(.+)__(.+)", testcase.get("name")).groups()
+    time = testcase.get("time")
+
+    rows.append((day, part, test, time * 1000))
+
+lines = [
+    "| Day | Part | Test | Time (ms) |\n",
+    "|-----|------|------|-----------|\n",
+]
+for row in rows:
+    lines.append(f"| {' | '.join(row)} |\n")
+
+with open("new-README.md", "w") as f:
+    f.writelines(lines)
