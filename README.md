b1a06c938a81a8119efe9c482b02ead2b52dcf6a
Lev <levatius@hotmail.co.uk>
2025-12-03 19:16:09 +0000

    Speed report testing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ![Tests Status Badge](https://github.com/Levatius/advent-of-code-2025/actions/workflows/python-tests.yml/badge.svg)
 
-![Tests Speed Report](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/Levatius/d6f958638db531ef1394f8e46954f787/raw/advent-of-code-2025-speed-report.json?nocache=1)
+![Speed Report](https://gist.githubusercontent.com/Levatius/2aec0c88c0ad990e4e1dcff16c004ed9/raw/report.md)
---
+++ scripts/generate_speed_report.py
@@ -0,0 +1,28 @@
+import re
+from xml.etree import ElementTree
+
+root = ElementTree.parse("report.xml").getroot()
+
+rows = []
+for testcase in root.iter("testcase"):
+  day = re.match(r"test.test__day_(.+)", testcase.get("classname")).group(1)
+  part, test = re.match(r"test__part_(.+)__(.+)", testcase.get("name")).groups()
+  time = testcase.get("time")
+  status = "passed"
+
+  if testcase.find("failure") is not None:
+      status = "failed"
+  elif testcase.find("skipped") is not None:
+      status = "skipped"
+
+  rows.append((day, part, test, status, time))
+
+lines = [
+    "| Day | Part | Test | Status | Time (s) |\n",
+    "|-----|------|------|--------|----------|\n",
+]
+for row in rows:
+  lines.append(f"| {" | ".join(row)} |\n")
+
+with open("report.md", "w") as f:
+    f.writelines(lines)
