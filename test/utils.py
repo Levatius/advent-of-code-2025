d77b252f8321596768bd00ba7865320ff21db83e
Lev <levatius@hotmail.co.uk>
2025-12-07 00:55:22 +0000

    Reverted utils update

--- test/utils.py
+++ test/utils.py
@@ -7,19 +7,21 @@ from aocd import get_data
 from aocd.exceptions import PuzzleLockedError
 
 
-def get_caller_file() -> str | None:
+def get_caller_path() -> Path:
     for frame_summary in traceback.extract_stack():
-        if "test__day" in (caller_file := frame_summary.filename):
-            return caller_file
-    return None
+        path = Path(frame_summary.filename)
+        if "test__day" in path.stem:
+            return path
 
 
 def get_aoc_imports():
-    caller_file = get_caller_file()
-    year_str, day_str = re.search(r"advent-of-code-(\d+)\\test\\test__day_(\d+).py", caller_file).groups()
-    module = importlib.import_module(f"src.day_{day_str}", str(Path.cwd() / "src" / f"day_{day_str}.py"))
+    caller_path = get_caller_path()
+    day = int(re.search(r"\d+", caller_path.stem)[0])
+    year = int(re.search(r"\d+", caller_path.parent.parent.stem)[0])
+    day_filename = f"day_{day:02}"
+    module = importlib.import_module(f"src.{day_filename}", str(Path.cwd() / "src" / f"{day_filename}.py"))
     try:
-        puzzle_input_lines = get_data(day=int(day_str), year=int(year_str)).splitlines()
+        puzzle_input_lines = get_data(day=day, year=year).splitlines()
     except PuzzleLockedError:
         puzzle_input_lines = None
     return module.parse, module.part_1, module.part_2, puzzle_input_lines
